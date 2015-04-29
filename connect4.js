f04b10e2414c5507f2a1ec81022554c2962f86b4
Alice Maz <alice.maz.martin@gmail.com>
2015-04-29 07:28:03 -0400

    minor cleanup, bout to do a major rewrite with the stream api

--- connect4.js
+++ connect4.js
@@ -2,26 +2,23 @@ var fs = require("fs");
 var api_stuff = JSON.parse(fs.readFileSync("config.json", "utf8"));
 
 var Twit = require("twit");
-var T = new Twit(
-{
+var T = new Twit({
 	consumer_key: api_stuff.key,
 	consumer_secret: api_stuff.secret,
 	access_token: api_stuff.token,
 	access_token_secret: api_stuff.token_secret
 });
 
-var wordfilter = require('wordfilter');
+var wordfilter = require("wordfilter");
 
 //some globals--this is the board, init to 0
 //ZERO ZERO IS THE TOP LEFT CORNER!!!!!
 //I hate doing it this way but otherwise looping in draw_board would be annoying
 //valid values: 0, "sun", "moon"
 var board_array = new Array(7);
-for(var i = 0; i < board_array.length; i++)
-{
+for(var i = 0; i < board_array.length; i++) {
 	board_array[i] = new Array(6);
-	for(var j = 0; j < board_array[i].length; j++)
-	{
+	for(var j = 0; j < board_array[i].length; j++) {
 		board_array[i][j] = 0;
 	}
 }
@@ -31,7 +28,7 @@ var to_play = "";
 //game type, "random", "speed", or "vote"
 var game_type = "";
 //id of most recent bot tweet, as a cutoff for on-turn moves
-var tweet_id = 0;
+var tweet_id = "";
 //number containing current move
 var current_move = 0;
 
@@ -40,45 +37,30 @@ String.prototype.capitalize = function() {
 }
 
 //game setup like first tweet, mode selection, stream opening, etc goes here
-function game_init()
-{
+function game_init() {
 	//choose random player to start
 	to_play = Math.floor(Math.random()*2) ? "sun" : "moon";
 	//replace with randomly chosen game type when I add more
 	game_type = "speed";
 
 	//tweet game start
-	T.post("statuses/update",
-	{
-		status: "Game Start\nMode: " + game_type.capitalize() + "\n\n" + draw_board() + "\n" + to_play.capitalize() + " to Play"
-	}, function(err, data, response)
-	{
-		console.log("made it to T.post's function");
-		//get this tweet's ID
-		T.get("statuses/user_timeline", { user_id: 3062270507, count: 1 }, function(err, data, response)
-		{
-			tweet_id = data[0].id;
-			console.log("made it to T.get's. id is " + tweet_id);
-			console.log("data.length: " + data.length);
-		});
+	T.post("statuses/update", {
+		status: "Game Start\nMode: " + game_type.capitalize() + "\n\n" + draw_board() + "\n" + to_play.capitalize() + " to Play"},
+		function(err, data, response) {
+			tweet_id = data.id_str;
 	});
 
-	console.log("out of T.post!");
 	current_move++;
 }
 
 //returns a string of newlined emoji repping the board plus column nums	
-function draw_board()
-{
+function draw_board() {
 	var board_img = "";
 
 	//sub w forEach later
-	for(var j = 0; j < 6; j++)
-	{
-		for(var i = 0; i < 7; i++)
-		{
-			switch(board_array[i][j])
-			{
+	for(var j = 0; j < 6; j++) {
+		for(var i = 0; i < 7; i++) {
+			switch(board_array[i][j]) {
 				case 0:
 					//blank
 					board_img += "\u2B1C";
@@ -110,8 +92,7 @@ function draw_board()
 //returns a number 0-6, a usable aray index for column 1-7, or -1 on failure
 //I think this is failure-proof, simply rejects anything that isn't an int 1-7 (0-6 after the -1 in move's assignment)
 //anyway it's a lazy match that grabs the first int 1-7
-function move_extract(the_tweet)
-{
+function move_extract(the_tweet) {
 	the_tweet = the_tweet.replace("@massconnect4 ","");
 	return (the_tweet.match(/[1-7]+?/) || [0])[0] - 1;
 }
@@ -123,16 +104,14 @@ function move_extract(the_tweet)
 //vote: figure out the most popular
 //in future also must make sure move is valid
 //think abt whether to silently pick another, or forfeit turn, on bad move
-function do_move(the_tweets)
-{
+function do_move(the_tweets) {
 	//for now, there are no modes
 	//this is now 0-6
 	//BIGBIGBIGBIGUGUGUIGIUG : it tweets the newest rather than oldest move?
 	//MUCH BIGGER BUG: it crashes if there's no tweets
 	var chosen_move, player;
 
-	switch(game_type)
-	{
+	switch(game_type) {
 		case "random":
 		case "speed":
 		case "vote":
@@ -143,10 +122,8 @@ function do_move(the_tweets)
 	if(wordfilter.blacklisted(player))
 		player = "********";
 
-	for(var j = 5; j > 0; j--)
-	{
-		if(board_array[chosen_move][j] === 0)
-		{
+	for(var j = 5; j > 0; j--) {
+		if(board_array[chosen_move][j] === 0) {
 			//put a sun or moon in chosen column on lowest free row
 			board_array[chosen_move][j] = to_play;
 			to_play = to_play === "sun" ? "moon" : "sun";
@@ -156,46 +133,33 @@ function do_move(the_tweets)
 
 	//we have now updated the board_array, turn, and current player
 	//in future, check for a win or draw now
-	T.post("statuses/update",
-	{
-		status: "Move " + current_move + ": " + player + " Plays " + (chosen_move+1) + "\n\n" + draw_board() + "\n" + to_play.capitalize() + " to Play"
-	}, function(err, data, response)
-	{
-		console.log("am i GETting or what");
-		//update the tweet id...
-		T.get("statuses/user_timeline", { user_id: 3062270507, count: 1 }, function(err, data, response)
-		{
-			console.log("yep!");
-			console.log("data id: " + data[0].id);
-			console.log("selftweet data: " + data);
-			tweet_id = data[0].id;
-		});
+	T.post("statuses/update", {
+		status: "Move " + current_move + ": " + player + " Plays " + (chosen_move+1) + "\n\n" + draw_board() + "\n" + to_play.capitalize() + " to Play"},
+		function(err, data, response) {
+			tweet_id = data.id_str;
 	});
 	current_move++;
 }
 
 //main function where shit happens
-function game_time()
-{
+function game_time() {
 	//init, obv
 	game_init();
 
 	//this block runs every 2 minutes
-	setInterval(function()
-	{
+	setInterval(function() {
 		//get 200 most recent statuses since last tweet
-		T.get("statuses/mentions_timeline", { count: 200, since_id: tweet_id }, function(err, data, response)
-		{
-			if(typeof data !== undefined)
-			{
-				//HELLO /!\ CHANGE THIS /!\
-				//make this an object instead of two random arrays
-				//I am too used to working around GML's bullshit and my bad habits have followed me to JS
+		T.get("statuses/mentions_timeline", { count: 200, since_id: tweet_id }, function(err, data, response) {
+			if(typeof data !== undefined) {
+				//this who function is garbage
+				//I should have a forEach, use obj of the form {alicemazzy:5, maliceazzy:2, ... etc }
+				//and just check if the entry exists rather than this filter nonsense
+				//HOWEVER
+				//game_time() itself needs to be totally revamped
+				//I want to use the streaming api, keep a buffer 
 				var tweets = [];
-				for(var i = 0; i < data.length; i++)
-				{
-					if(tweets.filter(function(element) { if(element.user === data[i].user.screen_name) return true; else return false;}).length === 0)
-					{
+				for(var i = 0; i < data.length; i++) {
+					if(tweets.filter(function(element) { if(element.user === data[i].user.screen_name) return true; else return false;}).length === 0) {
 						tweets.push({ user: data[i].user.screen_name, move: move_extract(data[i].text)});
 						console.log("user: " + tweets[tweets.length-1].user + "\nmove: " + tweets[tweets.length-1].move);
 					}
